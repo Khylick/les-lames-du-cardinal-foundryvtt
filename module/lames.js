7b3220f446abb3a329b57adbaa32d628235986ca
Khylick <contact.flambot@gmail.com>
2025-06-02 22:03:57 +0200

    Début ajout cases Vitalité/Tenacité

--- module/lames.js
+++ module/lames.js
@@ -5,7 +5,7 @@ Hooks.once("init", async function () {
 
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("les-lames-du-cardinal", LamesCharacterSheet, {
-    types: ["character"],
+    types: ["pj"],
     makeDefault: true
   });
 });
@@ -13,11 +13,18 @@ Hooks.once("init", async function () {
 class LamesActor extends Actor {
   prepareData() {
     super.prepareData();
-    // Ajoute ici tes calculs de données plus tard
+
+    // Initialise le tableau s’il n’existe pas déjà
+    if (!this.system.vitaliteTenacite || !Array.isArray(this.system.vitaliteTenacite)) {
+      this.updateSource({
+        "system.vitaliteTenacite": Array(8).fill("empty")
+      });
+    }
   }
 }
 
 class LamesCharacterSheet extends ActorSheet {
+
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["les-lames", "sheet", "actor"],
@@ -30,6 +37,36 @@ class LamesCharacterSheet extends ActorSheet {
 
   getData() {
     const context = super.getData();
+    context.system = this.document.system;
+
     return context;
   }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    html.find(".vitalite-tenacite .case").on("click", async (event) => {
+      const index = Number(event.currentTarget.dataset.index);
+      const actor = this.document;
+
+      // Copie du tableau actuel
+      const tableau = [...actor.system.vitaliteTenacite];
+
+      // Calcul du prochain état
+      let next = "empty";
+      if (tableau[index] === "empty") next = "checked";
+      else if (tableau[index] === "checked") next = "crossed";
+
+      // Mise à jour de l'élément cliqué
+      tableau[index] = next;
+
+      // Mise à jour de tout le tableau
+      await actor.update({
+        "system.vitaliteTenacite": tableau
+      });
+
+      this.render();
+    });
+  }
+
 }
